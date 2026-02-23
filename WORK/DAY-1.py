73214ab5fd27ff7325aba380bc44e18f125aa441
Aprajita <mishraaprajita43@gmail.com>
2026-02-23 12:57:00 +0530

    23/feb

--- WORK/DAY-1.py
+++ WORK/DAY-1.py
@@ -17,3 +17,48 @@ for i in range(n):
 
     for j in range(0, n):
         print(j)
+
+
+
+
+
+# '''
+#  Write a function that returns the first element of an array.         how to solve this q. ,
+# '''
+
+# def array_first_return(a):
+#     return a[0]
+
+# array = [1,2,3,4,5,6,4,5,6,7,8,4,3,2,24,4,32,2,2]
+
+# first_element = array_first_return(array)
+# print(first_element)
+'''
+O(1)
+'''
+
+n = int(input("Enter n: "))
+
+for i in range(1, 4):
+    print(i)
+    for j in range(1, n + 1):
+        print(j, end="\n\n")
+
+# O(n)
+'''
+n =2
+
+(1,3)
+1 and 2
+
+1
+2
+
+10
+O(3)
+
+
+
+
+O(1)
+'''
---
+++ WORK/DAY-2.py
@@ -0,0 +1,14 @@
+# arr = [1, 2, 3, 4, 5]
+
+# n = len(arr)
+
+# for i in range(n):
+#     print(arr[i])
+
+'''Check if a number is even or odd'''
+n = 5
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
