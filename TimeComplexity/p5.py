6a7c2971ca2124732878b41e474f079b0951e294
Aprajita <mishraaprajita43@gmail.com>
2026-05-09 13:37:45 +0530

    Logarthme

---
+++ TimeComplexity/p5.py
@@ -0,0 +1,5 @@
+n = 16
+
+while n > 1:
+    print(n)
+    n = n // 2
--- TimeComplexity/q1.py
+++ TimeComplexity/q1.py
@@ -5,3 +5,24 @@ for i in range(n):
         s += 1
         print(s)
         
+
+
+
+# O(1) Constant
+print("Hello")
+
+# O(n) Linear
+n = 5
+for i in range(n):
+    print(i)
+
+# O(n²) Quadratic
+for i in range(n):
+    for j in range(n):
+        print(i, j)
+
+# O(n³) Cubic
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            print(i, j, k)
