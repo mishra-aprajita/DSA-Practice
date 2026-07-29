bac40a5dca3460497d548e6461814769e667ad04
Aprajita <mishraaprajita43@gmail.com>
2026-07-29 21:23:30 +0530

    basic

---
+++ Strings/Basic Operations.py
@@ -0,0 +1,13 @@
+text = "Python"
+
+# 1. Reverse karna
+print(text[::-1])       # Output: nohtyP
+
+# 2. Length nikalna
+print(len(text))        # Output: 6
+
+# 3. Specific part nikalna (Slicing)
+print(text[0:2])        # Output: Py
+
+# 4. Check karna (Character hai ya nahi)
+print("P" in text)      # Output: True
